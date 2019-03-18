69deb657a249ef178cd278aace692fff5b2a9dd4
starbuck <mausuf@utexas.edu>
2019-03-18 15:02:55 -0500

    updated customer prompt initialization

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -20,7 +21,26 @@ connection.connect(function(err) {
   //Always keep /.end otherwise it will stay connected
   connection.query("select * from products", function(error,results) {
     if (error) throw error;
-    console.log(results);
-    connection.end();
+    var itemsForSale = results;
+    console.log(itemsForSale);
+
+    buyPrompt(itemsForSale);
   });
+
+  function buyPrompt() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to purchase?",
+        choices: [
+        ]
+      })
+
+  }
+
+
+
+
+  connection.end();
 });

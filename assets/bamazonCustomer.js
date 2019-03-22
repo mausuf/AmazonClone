ba8abce125d97f751b9c8b094556efd645b72159
starbuck <mausuf@utexas.edu>
2019-03-22 13:05:02 -0500

    js formatting update

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -1,25 +1,23 @@
+//-------------------------------------------------------
+// -------------------Setup------------------------------
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
 var connection = mysql.createConnection({
   host: "localhost",
-
   // Port
   port: 3306,
- 
   // Username
   user: "root",
-
   // Password
   password: "Qwerty555!",
   database: "bamazon"
 });
-
+//-------------------------------------------------------
+// ---------------------Connection-----------------------
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  //Always keep /.end otherwise it will stay connected
-  connection.query("select * from products", function(error,results) {
+  connection.query("select * from products", function(error, results) {
     if (error) throw error;
     var itemsForSale = results;
     console.log(itemsForSale);
@@ -28,19 +26,13 @@ connection.connect(function(err) {
   });
 
   function buyPrompt() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "rawlist",
-        message: "What would you like to purchase?",
-        choices: [
-        ]
-      })
-
+    inquirer.prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to purchase?",
+      choices: []
+    });
   }
-
-
-
-
+  //Always keep /.end otherwise it will stay connected
   connection.end();
 });

7a56606d855859ca53bdf5de21a15610b9d74ec8
starbuck <mausuf@utexas.edu>
2019-03-25 23:09:36 -0500

    updated table display

--- assets/bamazonCustomer.js
+++ assets/bamazonCustomer.js
@@ -17,50 +17,33 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  // connection.query("SELECT * FROM products", function(error, results) {
-  //   if (error) throw error;
 
-  //   for (var i = 0; i < results.length; i++) {
-  //     var itemsForSale = results[i].id;
-  //     var productName = results[i].product_name;
-  //     var departmentName = results[i].department_name;
-  //     var itemPrice = results[i].price;
-  //     var stockQuantity = results[i].stock_quantity;
-  //     console.log("Item#: " + itemsForSale);
-  //     console.log("Department: " + departmentName);
-  //     console.log("Product: " + productName);
-  //     console.log("Price: " + itemPrice);
-  //     console.log("Stock: " + stockQuantity);
-  //     console.log("-----------");
-  //   }
-
-  //   //Calling Inquirer Functions
-  //   // selectionPrompt(itemsForSale);
-  // });
-
-  // Puts items into Table
-  var table = function() {
+  // ---Show Inventory---
+  function showInventory(answer) {
     connection.query("SELECT * FROM products", function(error, results) {
       console.log("----------------------------------------------------");
+      // ---Create Table---
       for (var i = 0; i < results.length; i++) {
         console.log(
           results[i].id +
             " | " +
-          results[i].product_name +
+            results[i].department_name +
+            "|" +
+            results[i].product_name +
             " | $" +
-          results[i].price +
+            results[i].price +
             " | " +
-          results[i].stock_quantity
+            results[i].stock_quantity
         );
       }
       console.log("----------------------------------------------------");
       selectionPrompt();
-    });  
-  };
+    });
+  }
 
   //-------------------------------------------------------
   ////--------------Run bamazon App------------------------
-  table();
+  showInventory();
 
   //-------------------------------------------------------
   // ---------------------Inquirer-------------------------
